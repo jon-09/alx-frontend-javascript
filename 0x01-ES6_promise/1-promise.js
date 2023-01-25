268f10e47f3327a0f1135d34c4554fd8edab92fa
jon-09 <achola.jon@yahoo.com>
2023-01-25 18:06:26 +0300

    promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
-export default function getFullResponseFromAPI(success) {
-  const arr = new Promise((resolve, reject) => {
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
-  return arr;
 }
